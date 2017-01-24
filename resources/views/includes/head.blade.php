704a3d245fd90b5c3dcf54bc1e38d5c42454ee4c
Matt DePero <deperomm@miamioh.edu>
2017-01-23 19:44:43 -0500

    initial template set up

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,53 @@
+    <head>
+        <!-- Basic Page Needs
+        ================================================== -->
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="icon" type="image/png" href="images/favicon.png">
+        <title>Timer Agency Template</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="">
+        <!-- Mobile Specific Metas
+        ================================================== -->
+        <meta name="format-detection" content="telephone=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Template CSS Files
+        ================================================== -->
+        <!-- Twitter Bootstrs CSS -->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <!-- Ionicons Fonts Css -->
+        <link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}">
+        <!-- animate css -->
+        <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+        <!-- Hero area slider css-->
+        <link rel="stylesheet" href="{{ asset('css/slider.css') }}">
+        <!-- owl craousel css -->
+        <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/owl.theme.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/jquery.fancybox.css') }}">
+        <!-- template main css file -->
+        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+        <!-- responsive css -->
+        <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+        
+        <!-- Template Javascript Files
+        ================================================== -->
+        <!-- modernizr js -->
+        <script src="{{ asset('js/vendor/modernizr-2.6.2.min.js') }}"></script>
+        <!-- jquery -->
+        <script src="{{ asset('http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js') }}"></script>
+        <!-- owl carouserl js -->
+        <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+        <!-- bootstrap js -->
+
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+        <!-- wow js -->
+        <script src="{{ asset('js/wow.min.js') }}"></script>
+        <!-- slider js -->
+        <script src="{{ asset('js/slider.js') }}"></script>
+        <script src="{{ asset('js/jquery.fancybox.js') }}"></script>
+        <!-- template main js -->
+        <script src="{{ asset('js/main.js') }}"></script>
+    </head>
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+@include('includes.head')
+
+	<body>
+
+@include('includes.nav')
+
+@yield('content')
+
+@include('includes.footer')
+
+	</body>
+	
+</html>
